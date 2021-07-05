aef03aec5d9fce4aea43fc6287b9b9c973a1b72c
khoagman <69780742+khoagman@users.noreply.github.com>
2021-07-05 16:08:58 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,19 +4,20 @@ require "paq" {
     "savq/paq-nvim";                        -- Let Paq manage itself
     "rstacruz/vim-closer";	                -- Brackets closer	
     "gruvbox-community/gruvbox";            -- Color scheme
-    "itchyny/lightline.vim";	            -- Status bar
+    "itchyny/lightline.vim";
+   
     "junegunn/fzf";                         -- Finder
     "junegunn/fzf.vim";
     "neovim/nvim-lspconfig";                -- LSP
     "hrsh7th/nvim-compe";                   -- Auto complete
-    "hrsh7th/vim-vsnip";                    --snippet
-    "cdelledonne/vim-cmake";                --Cmake
+    "hrsh7th/vim-vsnip";                    -- Snippet
+    "cdelledonne/vim-cmake";                -- Cmake
 
-                                            --Telescope                 
+                                            -- Telescope                 
     "nvim-telescope/telescope.nvim";
     "nvim-lua/popup.nvim";
     "nvim-lua/plenary.nvim";
-    "voldikss/vim-floaterm";
+    "voldikss/vim-floaterm";                -- Floating terminal
 
 }
 
