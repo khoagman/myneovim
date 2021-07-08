bb4b979fae92b3b3d3d9be061e6450b814dde2eb
khoagman <khoagman@gmail.com>
2021-07-08 17:51:13 +0700

    update

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,9 +1,9 @@
---vim.o.background = "dark" -- or "light" for light mode
---vim.cmd([[colorscheme gruvbox]])
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
 --
 vim.g.gruvbox_italic = 1
-vim.g.gruvbox_improved_strings = 1
-vim.g.gruvbox_improved_warnings = 1
+--vim.g.gruvbox_improved_strings = 1
+--vim.g.gruvbox_improved_warnings = 1
 vim.g.gruvbox_contrast_light = 'hard'
 vim.g.gruvbox_contrast_dark = 'medium'
-vim.cmd [[colorscheme gruvbox]]
+--vim.cmd [[colorscheme gruvbox]]
